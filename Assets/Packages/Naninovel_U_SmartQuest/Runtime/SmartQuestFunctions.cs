014de052d6328f9c2a10d52998bc51d40cc69be4
Lesuut <113440589+Lesuut@users.noreply.github.com>
2025-02-10 19:56:23 +0200

    доделал чтобы работала фукнция проверки квеста на выполение

--- Assets/Packages/Naninovel_U_SmartQuest/Runtime/SmartQuestFunctions.cs
+++ Assets/Packages/Naninovel_U_SmartQuest/Runtime/SmartQuestFunctions.cs
@@ -3,11 +3,11 @@ namespace Naninovel.U.SmartQuest
     [ExpressionFunctions]
     public static class SmartQuestFunctions
     {
-        public static string GetQuestStatus()
+        public static bool GetQuestStatus(string idQuest)
         {
             var SmartQuestService = Engine.GetService<ISmartQuestService>();
 
-            return "";
+            return SmartQuestService.GetQuestStatus(idQuest);
         }
     }
 }
