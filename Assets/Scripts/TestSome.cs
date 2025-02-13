74389993d20b5a6bd797edad2a28e928e284adb2
Lesuut <113440589+Lesuut@users.noreply.github.com>
2025-02-13 19:27:01 +0200

    добавил кастомный принтер

--- Assets/Scripts/TestSome.cs
+++ Assets/Scripts/TestSome.cs
@@ -1,19 +1,21 @@
 using UnityEngine;
 using Naninovel;
-using Naninovel.U.Reception;
 
 public class TestSome : MonoBehaviour
 {
-    void Start()
+    void OnEnable()
     {
-        var ILocalizationManager = Engine.GetService<ILocalizationManager>();
+       /* var ILocalizationManager = Engine.GetService<ILocalizationManager>();
         var textManager = Engine.GetService<ITextManager>();
-        ILocalizationManager.OnLocaleChanged += (string locate) => Debug.Log(textManager.GetRecordValue("Reception.Screen.DateOfBirth.0", "Reception"));
+        ILocalizationManager.OnLocaleChanged += (string locate) => Debug.Log(textManager.GetRecordValue("Reception.Screen.DateOfBirth.0", "Reception"));*/
+
+        TestPrinter();
     }
 
     private void TestPrinter()
     {
+        Debug.Log("TestPrinter");
         var textPrinterManager = Engine.GetService<ITextPrinterManager>();
-        textPrinterManager.PrintTextAsync("Dialogue", "123", "Custom");
+        textPrinterManager.PrintTextAsync("ReceptionPrinter", "123", "Custom");
     }
 }
