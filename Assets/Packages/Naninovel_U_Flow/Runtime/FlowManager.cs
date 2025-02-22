2ec097dbe7a988f8dc14954e7ea73ac743ff759d
Lesuut <113440589+Lesuut@users.noreply.github.com>
2025-02-22 11:54:19 +0200

    сделал чтобы в флоу менеджере был запосной метод

--- Assets/Packages/Naninovel_U_Flow/Runtime/FlowManager.cs
+++ Assets/Packages/Naninovel_U_Flow/Runtime/FlowManager.cs
@@ -235,12 +235,20 @@ namespace Naninovel.U.Flow
                 StopFlow();
             }
         }
+        // Проигрывает команды асинхронно. Если это не работает в вашей версии, нужно выдрать код из класса PlayScript (namespace Naninovel)
+        // Или попробовать раскомитить код ниже
         private async void PlayScript(string scriptText)
         {
             var script = Script.FromScriptText($"Generated script", scriptText);
             var playlist = new ScriptPlaylist(script);
             await playlist.ExecuteAsync();
         }
+
+        /* protected virtual void PlayScript(string scriptText)
+        {
+            var player = Engine.GetService<IScriptPlayer>();
+            player.PlayTransient($"`Cross Promo` generated script", scriptText).Forget();
+        }*/
         private void TrySpawnFlowUI()
         {
             if (flowUI == null)
